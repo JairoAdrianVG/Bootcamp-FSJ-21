cb0d3cf2d9575f39dd01a4a7bee722512bdeff4f
JairoAdrianVG <jairoavegaromero@gmail.com>
2024-02-05 18:06:57 -0600

    Update Ecommerce Cart

--- Javascript/DOM/Ecommerce/js/main.js
+++ Javascript/DOM/Ecommerce/js/main.js
@@ -1,8 +1,9 @@
 const carrito = document.querySelector('#carrito');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const listaCursos = document.querySelector('#lista-cursos');
+let cursosCarrito = [];
 
-listaCursos.addEventListener('click',agregarCurso)
+//btnAgregar.addEventListener('click',agregarCurso)
 
 function agregarCurso(e){
     e.preventDefault()
@@ -12,28 +13,81 @@ function agregarCurso(e){
 }
 
 function leerDatosCurso(curso){
-    console.log(curso);
+   // console.log(curso.querySelector('a').getAttribute('data-id'));
 
+    //Objeto = {} atributos(caracteristicas) : VALOR A ASIGNAR
     const infoCurso = {
+        id: curso.querySelector('a').getAttribute('data-id'),
         imagen: curso.querySelector('img').src,
         titulo: curso.querySelector('h4').textContent,
-        precio: curso.querySelector('h5').textContent
+        precio: curso.querySelector('h5').textContent,
+        cantidad: 1
     }
 
-    console.log(infoCurso);
+    //Ver si el curso ya existe en el carrito
+    const existe = cursosCarrito.some( curso => curso.id === infoCurso.id)
+
+    if(existe){
+        cursosCarrito.map( curso => {
+            if(curso.id === infoCurso.id){
+                curso.cantidad += 1
+                return curso; //va a dejar el curso en el array actualizado(con cantidad +1).
+            }
+        });
+    } else{
+        cursosCarrito.push(infoCurso);
+    }
+    pintarCarritoHTML()
 }
 
-//infoCurso.imagen = "./images/curso1"
-//infoCurso.titulo = "HTML Y CSS"
-//infoCurso.precio = "$15.00"
+function pintarCarritoHTML(){
 
-// persona { }
+    //Limpiar el carrito
+    limpiarCarritoHTML();
 
-let persona = { 
-    ojos : "rojos",
-    cabello: "largo",
-    edad: 25,
-
+    //Metodo MAP arrays = Recorrer el array y devolver o ejecutar algo con cada posicion
+    cursosCarrito.map( (curso) => {
+        //Creamos el elemento tr de la tabla
+        const fila = document.createElement('tr');
+        //Asignamos un valor con innerHTML a esa fila
+        fila.innerHTML = `
+        <td> <img src="${curso.imagen}" width="100"> </td>
+        <td>${curso.titulo}</td>
+        <td>${curso.precio}</td>
+        <td>${curso.cantidad}</td>
+        <td><a href="#" onclick="eliminarConId(${curso.id})">X</a></td>
+        `                                                                           
+        
+    //Agrego la fila al contendor del carrito (tbody)
+    contenedorCarrito.appendChild(fila)
+    })
 }
 
-//Caracteristicao o atributo : VALOR
+function limpiarCarritoHTML() {
+    contenedorCarrito.innerHTML = ''; // Limpiar el contenido de una manera más directa
+}
+
+function vaciarCarrito(e){ 
+    e.preventDefault();
+    cursosCarrito = [];
+    limpiarCarritoHTML();
+}
+
+function eliminarConId(id) {
+  cursosCarrito.map(curso => {
+    if(curso.id == id ){
+        if(curso.cantidad > 1){
+            curso.cantidad -=1 
+            return curso
+        }else{
+            cursosCarrito = cursosCarrito.filter( curso => curso.id != id )
+        }
+        
+       
+    }
+  
+ })
+     
+ pintarCarritoHTML()
+
+}
