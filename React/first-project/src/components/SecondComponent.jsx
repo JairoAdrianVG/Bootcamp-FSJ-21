9fbc590a5d4f895ca4bd264466af4d8fb679834c
JairoAdrianVG <jairoavegaromero@gmail.com>
2024-02-21 21:18:23 -0600

    Estado y Props

--- React/first-project/src/components/SecondComponent.jsx
+++ React/first-project/src/components/SecondComponent.jsx
@@ -1,12 +1,27 @@
 import { useState } from "react"
+import { ThirdComponent } from "./ThirdComponent";
 
 export function SecondComponent() {
     //Declaracion de Estado
-    const [nombre,setNombre] = useState("Jairo");
+    const [nombre,setNombre] = useState("Jorge");
+
+    const cambiarEstado = () => {
+      console.log("Apretaste el boton");
+
+      if(nombre === "Jorge"){
+        setNombre("Chochan")
+      }else{
+        setNombre("Jorge")
+      }
+    }
 
   return (
     <>
     <h1>Soy el creador de esto y me llamo {nombre}</h1>
+    <br/>
+    <button onClick={cambiarEstado}>Apretame Pibe</button>
+
+    <ThirdComponent name={nombre}  />
     </>
   )
 }
@@ -14,3 +29,4 @@ export function SecondComponent() {
 //Estado -> Es la manera que tenemos de guardar informacion
 //Nos proporciona una manera de cambiar esa informacion
 //[NombreVariable,CambiarValorVariable] = useState(ValorInicial)
+
---
+++ React/first-project/src/components/ThirdComponent.jsx
@@ -0,0 +1,8 @@
+//Props -> Propiedades las cuales utilizamos para enviar informacion
+
+export const ThirdComponent = ({name}) => {
+
+  return (
+    <div>Soy el ThirdComponent y tu nombre es {name}</div>
+  )
+}
